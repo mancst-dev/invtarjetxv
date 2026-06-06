3beddf2772fc963705f9fc0ea4233876c8a6a839
mancst-dev <manuelcastano21@gmail.com>
2026-06-06 00:54:25 -0500

    upload files
    images

--- js/audio.js
+++ js/audio.js
@@ -20,39 +20,39 @@ btnPlayPause.addEventListener("click", () => {
   }
 });
 
-// 2. ACTUALIZAR LA BARRA Y EL TIEMPO CONFORME AVANZA LA CANCIÓN
+// actualizacion barra y tiempo cuando avanza la cancion
 audio.addEventListener("timeupdate", () => {
   if (audio.duration) {
     // Calcular el porcentaje completado para la barra
     const porcentaje = (audio.currentTime / audio.duration) * 100;
     barraProgreso.value = porcentaje;
 
-    // Actualizar el texto del tiempo actual transcurrido
+    // act texto del tiempo actual transcurrido
     tiempoActual.textContent = formatearTiempo(audio.currentTime);
   }
 });
 
-// 3. MOSTRAR LA DURACIÓN TOTAL CUANDO LA CANCIÓN CARGUE
+// mosrtr duracion total cancion crque
 audio.addEventListener("loadedmetadata", () => {
   tiempoTotal.textContent = formatearTiempo(audio.duration);
 });
 
-// 4. PERMITIR AL USUARIO MOVER LA BARRA PARA ADELANTAR/RETRASAR
+// interactuar con la barra
 barraProgreso.addEventListener("input", () => {
-  // Calcular el segundo exacto basado en el porcentaje de la barra
+  // clulo sequndo exacto de la brra
   const nuevoTiempo = (barraProgreso.value / 100) * audio.duration;
   audio.currentTime = nuevoTiempo;
 });
 
-// 5. FUNCIÓN AUXILIAR PARA PASAR SEGUNDOS A FORMATO "MINUTOS:SEGUNDOS" (e.g. 3:45)
+// funcion para pasar seundos a un formato 1:16
 function formatearTiempo(segundos) {
   const minutos = Math.floor(segundos / 60);
   const segRestantes = Math.floor(segundos % 60);
-  // Agrega un cero a la izquierda si los segundos son menores a 10
+  // add un cero a la izquierda si los segundos son menores a 10
   return `${minutos}:${segRestantes < 10 ? "0" : ""}${segRestantes}`;
 }
 
-// Extra: Si la canción termina, el botón vuelve a decir "Reproducir"
+// cuando la cancion termina valor de textcontent
 audio.addEventListener("ended", () => {
   btnPlayPause.textContent = "►";
   // btnPlayPause.style.backgroundColor = "#38bdf8";
